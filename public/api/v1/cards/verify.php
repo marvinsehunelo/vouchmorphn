02a524e9ba8cf14af374150ce5984e7b40e8793b
marvinsehunelo <marvinsehunelo@gmail.com>
2026-03-11 19:23:17 +0200

    Update verify.php

--- public/api/v1/cards/verify.php
+++ public/api/v1/cards/verify.php
@@ -8,8 +8,8 @@ header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, X-API-Key, X-Correlation-ID');
 
 // Fix the paths - remove one level of ../../
-require_once __DIR__ . '/../../../src/bootstrap.php';  
-require_once __DIR__ . '/../../../src/BUSINESS_LOGIC_LAYER/services/CardService.php';
+require_once __DIR__ . '/../../../../src/bootstrap.php';  
+require_once __DIR__ . '/../../../../src/BUSINESS_LOGIC_LAYER/services/CardService.php';
 
 use BUSINESS_LOGIC_LAYER\services\CardService;
 
