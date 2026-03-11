3e556fb8464bde38391ce153d5ca42dad2666647
marvinsehunelo <marvinsehunelo@gmail.com>
2026-03-11 19:16:05 +0200

    Update verify.php

--- public/api/v1/cards/verify.php
+++ public/api/v1/cards/verify.php
@@ -6,8 +6,8 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, X-API-Key, X-Correlation-ID');
 
-require_once __DIR__ . '/../../../bootstrap.php';
-require_once __DIR__ . '/../../../BUSINESS_LOGIC_LAYER/services/CardService.php';
+require_once __DIR__ . '/../../../src/bootstrap.php';
+require_once __DIR__ . '/../../../src/BUSINESS_LOGIC_LAYER/services/CardService.php';
 
 use BUSINESS_LOGIC_LAYER\services\CardService;
 use RuntimeException;
