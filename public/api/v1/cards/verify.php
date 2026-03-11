963ce7e43657e5cd67f2b1cd7f992a574ab0202d
marvinsehunelo <marvinsehunelo@gmail.com>
2026-03-11 19:25:51 +0200

    Update verify.php

--- public/api/v1/cards/verify.php
+++ public/api/v1/cards/verify.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, X-API-Key, X-Correlation-ID');
+header("Access-Control-Allow-Headers: Content-Type, X-API-Key, Authorization");
 
 // Fix the paths - remove one level of ../../
 require_once __DIR__ . '/../../../../src/bootstrap.php';  
