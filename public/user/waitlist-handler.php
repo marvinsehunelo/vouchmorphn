29232c53a2f1ca09895660b0c2e07a8e1252dcc8
marvinsehunelo <marvinsehunelo@gmail.com>
2026-04-10 15:42:23 +0200

    Create waitlist-handler.php

---
+++ public/user/waitlist-handler.php
@@ -0,0 +1,14 @@
+<?php
+// waitlist-handler.php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = $_POST['fullname'] ?? '';
+    $email = $_POST['email'] ?? '';
+    $country = $_POST['country'] ?? '';
+    $wallet = $_POST['primary_wallet'] ?? '';
+    
+    // Save to database or CSV or email
+    // For now, just redirect with success
+    header('Location: index.php?waitlist=success');
+    exit;
+}
+?>
